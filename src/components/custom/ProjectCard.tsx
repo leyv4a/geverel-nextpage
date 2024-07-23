ec8e540a98a78d1cb6fe124fe41f7d6114202d49
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-07-23 16:02:40 -0700

    feat: add beta projectCard

--- src/components/custom/ProjectCard.tsx
+++ src/components/custom/ProjectCard.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { cn } from "@/lib/utils";
 import Marquee from "@/components/magicui/marquee";
 import Image from 'next/image';
+import Link from 'next/link';
+import { Eye, EyeOff } from 'lucide-react';
 export default function ProjectCard({
     img,
     name,
-    username,
+    link,
     body,
   }: {
     img: string;
     name: string;
-    username: string;
+    link: string;
     body: string;
   }) {
    
@@ -24,16 +26,20 @@ export default function ProjectCard({
           "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
         )}
       >
+        {/* DETALLES */}
+        <blockquote className="my-2 text-sm">{body}</blockquote>
         <div className="flex flex-row items-center gap-2">
-          {/* <Image className="rounded-full" width="32" height="32" alt="" src={img} /> */}
-          <div className="flex flex-col">
+          <img className="rounded-full" width="32" height="32" alt="" src={img} />
+          <div className="flex flex-row justify-between w-full">
+            {/* CLIENTE */}
             <figcaption className="text-sm font-medium dark:text-white">
               {name}
             </figcaption>
-            <p className="text-xs font-medium dark:text-white/40">{username}</p>
+            {/* ENLACE */}
+            {/* <p className="text-xs font-medium dark:text-white/40">{link}</p> */}
+            <Link href={link} aria-disabled={link != 'none'? false : true} className={`${link != 'none'? ' ': 'pointer-events-none'}`} target='_blank' rel='noreferrer'>{link != 'none' ? <Eye/> : <EyeOff/>}</Link>
           </div>
         </div>
-        <blockquote className="mt-2 text-sm">{body}</blockquote>
       </figure>
     );
 }
--- src/components/custom/proyectos.tsx
+++ src/components/custom/proyectos.tsx
@@ -4,44 +4,20 @@ import ProjectCard from "./ProjectCard";
 export default function Proyectos() {
     const reviews = [
         {
-            name: "Jack",
-            username: "@jack",
-            body: "I've never seen anything like this before. It's amazing. I love it.",
-            img: "https://avatar.vercel.sh/jack",
+            name: "Serconomar",
+            link: "https://serconomar.com",
+            body: "Página web y bolsa de trabajo",
+            img: "https://serconomar.com/favico.png",
           },
           {
-            name: "Jill",
-            username: "@jill",
-            body: "I don't know what to say. I'm speechless. This is amazing.",
+            name: "Fruteria Taky",
+            link: "none",
+            body: "Punto de venta personalizado",
             img: "https://avatar.vercel.sh/jill",
-          },
-          {
-            name: "John",
-            username: "@john",
-            body: "I'm at a loss for words. This is amazing. I love it.",
-            img: "https://avatar.vercel.sh/john",
-          },
-          {
-            name: "Jane",
-            username: "@jane",
-            body: "I'm at a loss for words. This is amazing. I love it.",
-            img: "https://avatar.vercel.sh/jane",
-          },
-          {
-            name: "Jenny",
-            username: "@jenny",
-            body: "I'm at a loss for words. This is amazing. I love it.",
-            img: "https://avatar.vercel.sh/jenny",
-          },
-          {
-            name: "James",
-            username: "@james",
-            body: "I'm at a loss for words. This is amazing. I love it.",
-            img: "https://avatar.vercel.sh/james",
           }]
 
-        const firstRow = reviews.slice(0, reviews.length / 2);
-        const secondRow = reviews.slice(reviews.length / 2);
+        // const firstRow = reviews.slice(0, reviews.length / 2);
+        // const secondRow = reviews.slice(reviews.length / 2);
   return (
     <>
       <div className="h-[10rem] bg-gradient-to-b from-[#f2f2f2] to-transparent opacity-50"></div>
@@ -53,15 +29,15 @@ export default function Proyectos() {
         </h3>
         <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <Marquee pauseOnHover className="[--duration:20s]">
-        {firstRow.map((review) => (
-          <ProjectCard key={review.username} {...review} />
+        {reviews.map((review, index) => (
+          <ProjectCard key={index} {...review} />
         ))}
       </Marquee>
-      <Marquee reverse pauseOnHover className="[--duration:20s]">
-        {secondRow.map((review) => (
-          <ProjectCard key={review.username} {...review} />
+      {/* <Marquee reverse pauseOnHover className="[--duration:20s]">
+        {secondRow.map((review, index) => (
+          <ProjectCard key={index} {...review} />
         ))}
-      </Marquee>
+      </Marquee> */}
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
       <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
     </div>
