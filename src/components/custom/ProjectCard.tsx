7e680fc9d13080b3b2e35859ebd8325cb32a6d06
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-07-23 16:21:58 -0700

    feat: finish projectCard

--- src/components/custom/ProjectCard.tsx
+++ src/components/custom/ProjectCard.tsx
@@ -25,15 +25,16 @@ export default function ProjectCard({
           "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
           // dark styles
           "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
+          "h-auto"
         )}
       >
-        <div  className='flex flex-row w-full'>
-        <Image src={`/assets/projects/${img2}`} alt={img2} width={100} height={50}/>
-        <blockquote className="my-2 ms-1 text-left text-sm">{body}</blockquote>
+        <div  className='flex flex-row w-full h-[7s0%]'>
+        <Image src={`/assets/projects/${img2}`} alt={img2} width={300} height={150}/>
+        {/* <blockquote className=" ms-1 text-left text-sm">{body}</blockquote> */}
         </div>
         {/* DETALLES */}
   
-        <div className="flex flex-row items-center gap-2">
+        <div className="flex flex-row items-center gap-2 mt-2">
           <img className="rounded-full" width="32" height="32" alt="" src={img} />
           <div className="flex flex-row justify-between w-full">
             {/* CLIENTE */}
@@ -45,6 +46,7 @@ export default function ProjectCard({
             <Link href={link} aria-disabled={link != 'none'? false : true} className={`${link != 'none'? ' ': 'pointer-events-none'}`} target='_blank' rel='noreferrer'>{link != 'none' ? <Eye/> : <EyeOff/>}</Link>
           </div>
         </div>
+        <blockquote className="mt-1 ms-1 text-start text-gray-600 text-sm">{body}</blockquote> 
       </figure>
     );
 }
