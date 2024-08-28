a2cfd892a1f8661d31367f621c5ef7e3976db7ab
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-08-28 15:13:05 -0700

    resolve globals.css

--- src/app/cotizar/layout.tsx
+++ src/app/cotizar/layout.tsx
@@ -1,7 +1,7 @@
 
 import type { Metadata } from "next";
 import { Inter} from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Navbar from "../../components/custom/navbar";
 import {poppins, b612, allan} from '@/lib/fonts';
 import { Toaster } from "@/components/ui/toaster"
