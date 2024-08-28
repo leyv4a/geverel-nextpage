d567ceac4399e1fa684d494819ea8f86ac9f2a9a
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-08-28 14:02:44 -0700

    feat: complete SEO metadata

---
+++ src/app/cotizar/layout.tsx
@@ -0,0 +1,45 @@
+
+import type { Metadata } from "next";
+import { Inter} from "next/font/google";
+import "./globals.css";
+import Navbar from "../../components/custom/navbar";
+import {poppins, b612, allan} from '@/lib/fonts';
+import { Toaster } from "@/components/ui/toaster"
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Cotización",
+    description: "Solicita una cotización personalizada de Geverel Software para tus necesidades de desarrollo de software. ¡Contáctanos hoy mismo para empezar tu proyecto!",
+    keywords: "cotización de software, solicitud de presupuesto, desarrollo de software a medida",
+    authors: [{ name: 'Geverel Software' }],
+    openGraph: {
+      title: "Cotización",
+      description: "Solicita una cotización personalizada de Geverel Software para tus necesidades de desarrollo de software. ¡Contáctanos hoy mismo para empezar tu proyecto!",
+      url: 'https://www.geverel.com/cotizar',
+      type: 'website',
+    },
+    robots: {
+      index: true,
+      follow: true,
+    },
+    alternates: {
+      canonical: 'https://www.geverel.com/cotizar',
+    },
+  };
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <body className={`${inter.className} ${b612} ${poppins} ${allan}`}>
+        <Navbar/>
+        {children}
+        <Toaster />
+        </body>
+      </html>
+    );
+  }
